bd539b134f8456fed567ea139e2645405e5ceca7
richvillafana <richard.villafana@afs.com>
2022-09-15 19:57:10 +0000

    fix server py

--- server.py
+++ server.py
@@ -213,10 +213,10 @@ def show_account():
 def show_locations():
     """Show local pickup locations"""
 
-if os.environ["LOCATION_FEATURE_ENABLED"] == "True":
+    if os.environ["LOCATION_FEATURE_ENABLED"] == "True":
         pickups = db.session.query(Pickup).filter(Pickup.pickup_id > 1).all()
         return render_template("locations.html", pickups=pickups)
-else:
+    else:
         return render_template("coming-soon.html")
 
 @app.route('/cart')
